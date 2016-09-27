59ef818122b289a5cf50429611b46548dd806b7b
yessica ancan <yancalonzo@gmail.com>
2016-09-27 18:20:49 -0300

    Arreglos varios estilos y html

---
+++ css/datos.php
@@ -0,0 +1,8 @@
+<?php
+
+	/*Recopilando datos del formulario*/
+    $email=$_POST["Departamento"];
+    email($email, $subject, $msg, "FROM: yancalonzo@gmail.com<yancalonzo@gmail.com>\n")
+    
+
+?>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 
 $(document).ready(function(){
-	$(".navbar").click(function(){
-		$(".navbar").animate({background-color:"rgb(33, 148, 120)", display:"block"});
+	$("#benviar").click(function(){
+		var nombre=$("#name").val();
+		var correo=$("#email").val();
+		var asunto=$("#comment").val();
+
+		if(nombre==""){
+			$("mensaje").fadeIn();
+			return false;
+		}else{
+			$("mensaje").fadeOut();
+			if(correo==""||expr.test(correo)){
+				$("mensaje2").fadeIn();
+				return false;
+			}else{
+				$("mensaje2").fadeOut();
+
+				if(asunto==""){
+					$("mensaje2").fadeIn();
+				return false;
+				}
+			}
+		}
 	});
 });
-
-
-
